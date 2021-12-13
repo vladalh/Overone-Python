f21962dc89d6f85732df85106e22e91b720a7f7b
Uladzimir Al'khimovich <58719393+vladalh@users.noreply.github.com>
2021-12-13 13:58:28 +0300

    Add files via upload

---
+++ eratosthenes2.py
@@ -0,0 +1,12 @@
+n = int(input("Enter any number:"))
+even = 0
+odd = 0
+
+for i in n:
+    if int(i) != j and j != 1:
+            if  not i % j:
+                break
+    else:
+        print(i, end=" ")
+
+# vladalh@mail.ru
