060090f2f408bb7282e79e5089a52d829d3c13b8
Uladzimir Al'khimovich <58719393+vladalh@users.noreply.github.com>
2021-12-18 20:42:54 +0300

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -1,23 +1,31 @@
-# Enter a math expression separated by a space
-exp = input("Enter math expression :").split()
 
-# Getting ahead of the first and second numbers
-number1 = int(exp[0])
-number2 = int(exp[2])
 
-# Defining a math action
-result = ""
-if exp[1] == '+':
-    result = "{0:.3f}".format(number1 + number2)
-elif exp[1] == '-':
-    result = "{0:.3f}".format(number1 - number2)
-elif exp[1] == '/' and number2 != 0:
-    result = "{0:.3f}".format(number1 / number2)
-elif exp[1] == '*':
-    result = "{0:.3f}".format(number1 * number2)
-elif exp[1] == '**':
-    result = "{0:.3f}".format(number1 ** number2)
-print("Expression result :", result)
+def result(x, y, sign):
+    if sign == '+':
+        result = x + y
+    elif sign == '-':
+        result = x - y
+    elif sign == '/':
+        result = x / y
+    elif sign == '*':
+        result = x * y
+    elif sign == '**':
+        result = x ** y
+    return result
 
+
+try:
+
+    # Enter a math expression separated by a space
+    exp = input("Enter math expression :").split()
+
+    # Getting ahead of the first and second numbers
+    number1 = int(exp[0])
+    number2 = int(exp[2])
+
+    print("{0:.3f}".format(result(number1, number2, exp[1])))
+
+except ZeroDivisionError:
+    print("Division by zero!")
 
 # vladalh@mail.ru
