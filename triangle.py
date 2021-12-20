76697af388186ec153dfb5687630b6b461c6e7f8
Uladzimir Al'khimovich <58719393+vladalh@users.noreply.github.com>
2021-12-20 22:21:15 +0300

    Add files via upload

--- triangle.py
+++ triangle.py
@@ -1,13 +1,17 @@
+def triangle(a, b, c):
+    if a + b > c and a + c > b and b + c > a:
+        return "The figure is a triangle"
+    else:
+        return "The shape is not a triangle"
+
+
 # the user enters the lengths of the sides
 
 a = float(input("Enter side A :"))
 b = float(input("Enter side B :"))
 c = float(input("Enter side C :"))
 
-# determine if a shape with the specified sides is a triangle
-if a + b > c and a + c > b and b + c > a:
-    print("The figure is a triangle")
-else:
-    print("The shape is not a triangle")
+
+print(triangle(a, b, c))
 
 # vladalh@mail.ru
